8bf6546f03c153bbe19fdf8b5d5c3c5270fb9bf0
Mary Grace Nazareno <mgracenazareno@gmail.com>
2021-02-11 19:12:29 -0500

    Add params to def answer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,19 +1,11 @@
 class QuestionsController < ApplicationController
-  attr_accessor :message, :answer
+  # attr_accessor :message, :answer
 
   def ask
-    @message
+    @message = params[:message]
   end
 
   def answer
-    @answer
-    # raise
-    # if params[:@message]
-    #   "Great!"
-    # elsif params[:@message].end_with?("?")
-    #   "Silly question, get dressed and go to work!"
-    # else
-    #   "I don't care, get dressed and go to work!"
-    # end
+    @answer = params[:answer]
   end
 end
