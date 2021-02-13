f34efee266cb6f1c30367ba8bb8c7075ceb1b6ee
Mary Grace Nazareno <mgracenazareno@gmail.com>
2021-02-12 22:10:45 -0500

    Display answer but not the right one

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,29 @@
-class QuestionsController < ApplicationController
-  # attr_accessor :message, :answer
+# frozen_string_literal: true
 
+class QuestionsController < ApplicationController
   def ask
     @message = params[:message]
   end
 
   def answer
-    @answer = params[:answer]
+    @question = params[:question]
+    if @message.blank?
+      @answer = "I can't hear you!"
+    elsif @message == 'I am going to work'
+      @answer = 'Great!'
+    elsif @message.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      "I don\t care, get dressed and go to work!"
+    end
+    # if @question.blank?
+    #   @answer = "I can't hear you!"
+    # elsif @question == 'I am going to work'
+    #   @answer = 'Great!'
+    # elsif @question.end_with?('?')
+    #   @answer = 'Silly question, get dressed and go to work!'
+    # else
+    #   "I don\t care, get dressed and go to work!"
+    # end
   end
 end
